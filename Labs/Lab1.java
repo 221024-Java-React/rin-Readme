aca2a572b6af850b7d48e6b4efd1dbf3034972b6
coopp <coopplmedia@gmail.com>
2022-10-25 10:34:25 -0700

    Lab1

---
+++ Labs/Lab1.java
@@ -0,0 +1,19 @@
+public class Lab1{
+	public static void main(String[] args){
+		boolean boo = true;
+		byte maxByte = 127;
+		short minShort = -32768;
+		int maxInt = 2147483647;
+		long javaFlex = 3000000000L;
+		float pie = 3.1415F;
+		double doubleYum = 6.283D;
+ 
+		System.out.println("boo: " + boo);
+		System.out.println("maxByte: " + maxByte);
+		System.out.println("minShort: " + minShort);
+		System.out.println("maxInt: " + maxInt);
+		System.out.println("javaFlex: " + javaFlex);
+		System.out.println("pie: " + pie);
+		System.out.println("doubleYum: " + doubleYum);
+	}
+}
